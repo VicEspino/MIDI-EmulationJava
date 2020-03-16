a0010795ca2e28327e9744d0564d859c469542c4
VicEspino <taxgype1@gmail.com>
2020-03-15 20:14:11 -0600

    REceiver

--- src/midi/FXMLDocumentController.java
+++ src/midi/FXMLDocumentController.java
@@ -7,15 +7,22 @@ package midi;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javax.sound.midi.Instrument;
+import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiChannel;
+import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Receiver;
+import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Synthesizer;
+import javax.sound.midi.Transmitter;
 
 /**
  *
@@ -30,13 +37,55 @@ public class FXMLDocumentController implements Initializable {
     private void handleButtonAction(ActionEvent event) throws MidiUnavailableException {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
-        Synthesizer siSynthesizer = MidiSystem.getSynthesizer();
-        siSynthesizer.open();
-        MidiChannel[] midiChanels = siSynthesizer.getChannels();
+//        Synthesizer siSynthesizer = MidiSystem.getSynthesizer();
+//        siSynthesizer.open();
+//        MidiChannel[] midiChanels = siSynthesizer.getChannels();
+//        
+//        Instrument[] instruments = siSynthesizer.getDefaultSoundbank().getInstruments();
+//        siSynthesizer.loadInstrument(instruments[0]);
+    
+        Receiver rcv= MidiSystem.getReceiver();
+//        Transmitter transmitter = MidiSystem.getTransmitter();
+//        transmitter.setReceiver(rcv);
         
-        Instrument[] instruments = siSynthesizer.getDefaultSoundbank().getInstruments();
-        siSynthesizer.loadInstrument(instruments[0]);
+        MidiDevice.Info[] midiDeviceInfo = MidiSystem.getMidiDeviceInfo();
+        
+        for(MidiDevice.Info inforact:midiDeviceInfo){
+
+            System.out.println(inforact.toString());
+            System.out.println(inforact.getDescription()+"\n");
+
+        }        
+        
+        MidiDevice midiDevice = MidiSystem.getMidiDevice(midiDeviceInfo[4]);
+       // midiDevice.getReceivers().add(rcv);
+    
+        if(midiDevice.isOpen()){
+                  
+        }else{
+            midiDevice.open();
+            
+        }
+        
+        
+        ShortMessage msMessage = new ShortMessage();
+        try {
+            msMessage.setMessage(ShortMessage.NOTE_ON, 0, 20, 93);
+            rcv.send(msMessage, -1);
+            
+            System.out.println("Channel-> "+msMessage.getChannel());
+
+ 
+
+            
+            
+        } catch (InvalidMidiDataException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+       
     }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
